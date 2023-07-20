0c4fd94c016ec2e7476ce77df9913ea2a9460a3d
everinurmind <nurbol.sultanov@gmail.com>
2023-07-20 15:58:13 -0600

    Setup routes / Update db

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,19 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'splash#index'
+
+  get 'splash', to: 'splash#index', as: 'splash'
+  get 'login', to: 'sessions#new', as: 'log_in'
+  post 'login', to: 'sessions#create'
+  get 'sign_in', to: 'sessions#new', as: 'sign_in'
+  post 'sign_in', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy', as: 'log_out'
+  get 'sign_up', to: 'users#new', as: 'sign_up'
+  post 'sign_up', to: 'users#create'
+
+  resources :categories, only: [:index, :new, :create, :destroy, :show] do
+    resources :purchases, only: [:show, :new, :create]
+    resources :category_transactions, only: [:create, :show, :new]
+  end
 end
